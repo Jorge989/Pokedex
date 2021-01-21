8c2a362da3fe7995eea07efc0024c16eac0fc269
Jorge989 <attiejorge89@gmail.com>
2021-01-21 20:28:25 -0300

    first commit

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+
+
+
+    <div class="card">
+      <div class="card-image">
+        <figure >
+          <img
+            :src="pokemon.front"
+            alt="Placeholder image"
+          />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+     
+          <div class="media-content">
+            <p class="title is-4">{{ num }} - {{ name | upper }}</p>
+            <p class="subtitle is-6">{{pokemon.type}}</p>
+          </div>
+        </div>
+
+        <div class="content">
+      
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  created: function() {
+    axios.get(this.url).then((res) => {
+      this.pokemon.type = res.data.types[0].types.name;
+      this.pokemon.front = res.data.sprites.front_default;
+      this.pokemin.back = res.data.sprites.back_default;
+    });
+  },
+  data() {
+    return {
+      pokemon: {
+          front:"",
+            back:"",
+      },
+    };
+  },
+  props: {
+    num: Number,
+    name: String,
+    url: String,
+  },
+  filters: {
+    upper: function(value) {
+      var newName = value[0].toUpperCase() + value.slice(1);
+
+      return newName;
+    },
+  },
+};
+</script>
+
+<style></style>
